0d1048928b3f768248587c1fbb4160c4f8ce8036
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-01-23 22:18:29 +0100

    myApp.js

--- myApp.js
+++ myApp.js
@@ -1,14 +1,22 @@
+//variable declations
 var express = require('express');
 var app = express();
+let absolutPath = __dirname + '/views/index.html'
+let stylePath = __dirname + '/public/style.css';
+
+
+//sending files
 app.get('/', (req, res) => {
-    let absolutPath = __dirname + '/views/index.html'
     res.sendFile(absolutPath);
 });
 
+app.use(stylePath, express.static(
+    
+))
 
 
 
-
+console.log('Hello World');
 
 
 
